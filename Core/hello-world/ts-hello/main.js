9727f5e2b87083d0baad5b603ec856719525ed3c
themaryanjuguna <themaryanjuguna@gmail.com>
2022-04-16 00:34:51 +0300

    type assertions

--- Core/hello-world/ts-hello/main.js
+++ Core/hello-world/ts-hello/main.js
@@ -1,7 +1,16 @@
-function doSomething() {
-    for (var i = 0; i < 5; i++) {
-        console.log(i);
-    }
-    console.log('finally: ' + i);
-}
-doSomething();
+var a;
+var b;
+var c;
+var d;
+var e = [1, 2, 3];
+var f = [1, true, 'a', false];
+var ColorRed = 0;
+var ColorGreen = 1;
+var ColorBlue = 2;
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+})(Color || (Color = {}));
+var backgroundColor = Color.Blue;
--- Core/hello-world/ts-hello/main.ts
+++ Core/hello-world/ts-hello/main.ts
@@ -1,15 +1,2 @@
-let a: number;
-let b: boolean;
-let c: string;
-let d: any;
-let e: Number [] = [1, 2, 3];
-let f: any[] = [1, true, 'a', false];
-
-
-const ColorRed = 0;
-const ColorGreen = 1;
-const ColorBlue =2;
-
-
-enum Color {Red = 0, Green = 1, Blue = 2}
-let backgroundColor = Color.Blue;
+let message = 'abc' //string
+let endWithC = message.endsWith('c');
