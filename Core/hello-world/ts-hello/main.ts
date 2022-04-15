7ca9fd619201d34bad9abc8dc4f7063e5358a8d2
themaryanjuguna <themaryanjuguna@gmail.com>
2022-04-16 00:26:08 +0300

    data type

--- Core/hello-world/ts-hello/main.ts
+++ Core/hello-world/ts-hello/main.ts
@@ -1,9 +1,15 @@
-function doSomething(){
-    for (var i = 0; i < 5; i++){
-        console.log(i);
-    }
+let a: number;
+let b: boolean;
+let c: string;
+let d: any;
+let e: Number [] = [1, 2, 3];
+let f: any[] = [1, true, 'a', false];
 
-    console.log('finally: ' + i);
-}
 
-doSomething();
+const ColorRed = 0;
+const ColorGreen = 1;
+const ColorBlue =2;
+
+
+enum Color {Red, Green, Blue}
+let backgroundColor = Color.Blue;
