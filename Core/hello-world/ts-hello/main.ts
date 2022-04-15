35d52cad08c7d229d86ed6329005d0f15092360d
themaryanjuguna <themaryanjuguna@gmail.com>
2022-04-16 01:09:11 +0300

    interfaces - inline annotation

--- Core/hello-world/ts-hello/main.ts
+++ Core/hello-world/ts-hello/main.ts
@@ -8,6 +8,11 @@ let drawPoint = (point: (point: Point) => {    //inline annotation
     //....
 }
 
+
+let getDistance = (pointA: Point, PointB: Point) => {
+    // ...
+}
+
 drawPoint({
     x: 1,
     y: 2
