d60086d0054379bec2e027a83b3bb551fdf1086c
themaryanjuguna <themaryanjuguna@gmail.com>
2022-04-16 21:22:03 +0300

    angular templates - Method

--- Core/hello-world/src/app/courses.component.ts
+++ Core/hello-world/src/app/courses.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'courses',
-    template: '<h2>{{ "Title:" + title }}</h2>'
+    template: '<h2>{{ getTitle() }}</h2>'
 })
 export class CoursesComponent {
     title = "List of courses";
+
+    getTitle() {
+        return this.title;
+    }
 }
