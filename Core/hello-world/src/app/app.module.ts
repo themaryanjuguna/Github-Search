b9caf89d7f1c44a5a86b7699713cb54ad23b01bb
themaryanjuguna <themaryanjuguna@gmail.com>
2022-04-16 21:20:24 +0300

    angular templates

--- Core/hello-world/src/app/app.module.ts
+++ Core/hello-world/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { CoursesComponent } from './courses.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -5,9 +6,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CourseComponent } from './course/course.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
+    CoursesComponent,
     CourseComponent
   ],
   imports: [
