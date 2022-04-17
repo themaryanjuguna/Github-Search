b195101330bbddd9c7e859db509e03e98e1a09ef
themaryanjuguna <themaryanjuguna@gmail.com>
2022-04-17 16:29:10 +0300

    Styling Layout

---
+++ src/app/dropdown-navbar.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'ngbd-dropdown-navbar',
+  templateUrl: './navbar/navbar.component.html'
+})
+export class NgbdDropdownNavbar {
+  collapsed = false;
+}
--- src/app/github-main/github-main.component.ts
+++ src/app/github-main/github-main.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-github-main',
   templateUrl: './github-main.component.html',
