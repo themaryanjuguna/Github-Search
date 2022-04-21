0695ef83c268614bf060622f4af03d08d5493df1
themaryanjuguna <themaryanjuguna@gmail.com>
2022-04-21 14:26:53 +0300

    Layout adjustments

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ import { GitProfileComponent } from './git-profile/git-profile.component';
 const routes: Routes = [
   { path: 'git-profile', component: GitProfileComponent  },
   { path:'**', component:NotFoundComponent},
-  { path: '', redirectTo:"/git-profile", pathMatch:"full"},
+  { path: '', redirectTo:"/not-found", pathMatch:"full"},
 ];
 
 @NgModule({
