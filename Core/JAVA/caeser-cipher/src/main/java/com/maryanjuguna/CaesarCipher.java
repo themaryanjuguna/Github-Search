3eb17ae67822f078bd08d32f6ac3e317905996e3
themaryanjuguna <themaryanjuguna@gmail.com>
2022-04-24 00:53:29 +0300

    initial commit

---
+++ Core/JAVA/caeser-cipher/src/main/java/com/maryanjuguna/CaesarCipher.java
@@ -0,0 +1,22 @@
+package com.maryanjuguna;
+
+public class CaesarCipher {
+
+    static String alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+    public static String encoding(String plainText, int Key) {
+        plainText = plainText.toLowerCase();
+        String cipherText = "";
+
+
+        return cipherText;
+
+    }
+
+    public static String encoding() {
+
+    }
+
+
+
+}
