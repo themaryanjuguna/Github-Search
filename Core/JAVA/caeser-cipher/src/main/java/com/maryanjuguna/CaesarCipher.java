8bd7c7945bc44be9240ade9f993932ba1403cbb1
themaryanjuguna <themaryanjuguna@gmail.com>
2022-04-24 01:10:33 +0300

    validation of user input btwn 0 - 25

--- Core/JAVA/caeser-cipher/src/main/java/com/maryanjuguna/CaesarCipher.java
+++ Core/JAVA/caeser-cipher/src/main/java/com/maryanjuguna/CaesarCipher.java
@@ -7,6 +7,14 @@ public class CaesarCipher {
     public static String encoding(String plainText, int Key) {
         plainText = plainText.toLowerCase();
         String cipherText = "";
+        for (int i = 0; i<plainText.length(); i++) {
+            //find out index of the alphabet
+            int charIndex = alphabet.indexOf(plainText.charAt(i));
+            //generate a new index. key shift value
+            int newIndex = (charIndex + Key) % 26;
+            Char cipherChar = alphabet.charAt(newIndex);
+
+        }
 
 
         return cipherText;
