68b8aca36057aa9452fe53dcba6c49e6d11c08a5
themaryanjuguna <themaryanjuguna@gmail.com>
2022-04-30 23:22:23 +0300

    form and greeting_card.hbs

--- Core/JAVA/friendletter/src/main/java/App.java
+++ Core/JAVA/friendletter/src/main/java/App.java
@@ -12,13 +12,27 @@ public class App {
         staticFileLocation("/public");
 
         get("/", (request, response) ->{ //request for route happens at this location
-            Map<String, Object> model = new HashMap<String, Object>(); // new model is made to store information
+            Map<String, Object> model = new HashMap<>(); // new model is made to store information
             return new ModelAndView(model, "hello.hbs"); // assemble individual pieces and render
                 }, new HandlebarsTemplateEngine());
 
         get("/favorite_photos",(request, response) ->{
-            Map<String, Object> model = new HashMap<String, Object>();
+            Map<String, Object> model = new HashMap<>();
             return  new ModelAndView(model, "favorite_photos.hbs");
                 }, new HandlebarsTemplateEngine());
+
+        get("/form",(request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "form.hbs");
+              }, new HandlebarsTemplateEngine());
+
+        get("/greeting_card", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            String recipient = request.queryParams("recipient");
+            String sender = request.queryParams("sender");
+            model.put("recipient", recipient);
+            model.put("sender", sender);
+            return new ModelAndView(model, "greeting_card.hbs");
+            }, new HandlebarsTemplateEngine());
     }
 }
