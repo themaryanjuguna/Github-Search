e322cbb4b70af6514e4c07277ad4503c08039bba
themaryanjuguna <themaryanjuguna@gmail.com>
2022-05-01 13:40:11 +0300

    Working with time in Java

---
+++ Core/JAVA/Clock/src/main/java/App.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+//Example One
+public class App {
+    public static void main(String[] args) {
+        //new date subject
+        Date newDate = new Date();
+        //Display time and date using toString()
+        System.out.println(newDate.toString());
+    }
+}
