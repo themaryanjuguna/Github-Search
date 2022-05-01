09307e1c42a256dcfb7ae68ac36094dc9738628f
themaryanjuguna <themaryanjuguna@gmail.com>
2022-05-01 15:00:04 +0300

    Example 2

--- Core/JAVA/Clock/src/main/java/App.java
+++ Core/JAVA/Clock/src/main/java/App.java
@@ -1,11 +1,11 @@
+import java.text.SimpleDateFormat;
 import java.util.Date;
-
-//Example One
+//Example two
 public class App {
+
     public static void main(String[] args) {
-        //new date subject
-        Date newDate = new Date();
-        //Display time and date using toString()
-        System.out.println(newDate.toString());
+        Date secondDate = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("E. MMMM d 'at' hh:mm a");
+        System.out.println("Current Date: " + sdf.format(secondDate)); //Apply Pattern to data
     }
 }
