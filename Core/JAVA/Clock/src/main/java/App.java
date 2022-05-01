750157d4ab8a3da10dae10d1ecf06bee2303fbf6
themaryanjuguna <themaryanjuguna@gmail.com>
2022-05-01 15:28:58 +0300

    Example four

--- Core/JAVA/Clock/src/main/java/App.java
+++ Core/JAVA/Clock/src/main/java/App.java
@@ -1,11 +1,25 @@
+import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 //Example two
 public class App {
 
-    public static void main(String[] args) {
-        Date secondDate = new Date();
-        SimpleDateFormat sdf = new SimpleDateFormat("E. MMMM d 'at' hh:mm a");
-        System.out.println("Current Date: " + sdf.format(secondDate)); //Apply Pattern to data
+    public static void main(String args[]) {
+        String startDateString = "07/7/2017";
+        DateFormat df = new SimpleDateFormat("MM/dd/yyyy");
+        Date startDate;
+        {
+            try {
+                startDate = df.parse(startDateString);
+                SimpleDateFormat ft = new SimpleDateFormat("E,MMMM d"); // formatter pattern
+                System.out.println("Date that was formerly a String, formatted: " + ft.format(startDate));
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+
+        }
+
     }
 }
+
