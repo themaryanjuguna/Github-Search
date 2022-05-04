62e0b7a073fea99ec2385690ec1d6ae0df292132
themaryanjuguna <themaryanjuguna@gmail.com>
2022-05-04 13:29:48 +0300

    Deployment to heroku achieved. styling update

---
+++ Core/JAVA/HeroSquad/src/main/java/App.java
@@ -0,0 +1,75 @@
+import models.hero;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import static spark.Spark.*;
+
+public class App {
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
+    public static void main(String[] args) {
+
+        port(getHerokuAssignedPort());
+        staticFileLocation("/public");
+
+
+
+
+
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            ArrayList<hero> heroes = hero.getAll();
+            model.put("heroes", heroes);
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+
+        post("/heroes/new", (request, response) ->{
+            Map<String, Object> model = new HashMap<>();
+            Set<String> heroNo = request.queryParams();
+            String name = request.queryParams("name");
+            String dob = request.queryParams("dob");
+            String strength = request.queryParams("strength");
+            String weakness = request.queryParams("weakness");
+            String chooseSquad = request.queryParams("chooseSquad");
+
+
+            hero newHero = new hero(name, dob, strength, weakness, chooseSquad, 1);
+            return new ModelAndView(model, "success.hbs");
+        }, new HandlebarsTemplateEngine());
+
+
+        get("/shujaa",(request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            ArrayList<hero> heroes = hero.getAll();
+            String name = request.queryParams("name");
+            String dob = request.queryParams("dob");
+            String strength = request.queryParams("strength");
+            String weakness = request.queryParams("weakness");
+            String chooseSquad = request.queryParams("chooseSquad");
+
+            model.put("name", name);
+            model.put("dob", dob);
+            model.put("strength", strength);
+            model.put("weakness", weakness);
+            model.put("chooseSquad", chooseSquad);
+
+            return new ModelAndView(model, "shujaa.hbs");
+        }, new HandlebarsTemplateEngine());
+    }
+
+
+
+}
+
---
+++ Core/JAVA/HeroSquad/src/test/java/AppTest.java
@@ -0,0 +1,14 @@
+/*
+import junit.framework.TestCase;
+
+
+public class AppTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void tearDown(){
+    }
+}
+*/
